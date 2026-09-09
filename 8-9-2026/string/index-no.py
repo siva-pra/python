82d406358187b24f1a398275e3e15cd650e05419
Sivaprasad Tellagorla <sivaprasad@buildbot.tech>
2026-09-09 11:54:00 +0530

    string

---
+++ 8-9-2026/string/index-no.py
@@ -0,0 +1,28 @@
+## 
+#-10
+# s   i v a p r a s a d
+# 0 1 2 3 4 5 6 7 8 9
+# print the character at index 0 to 9 and -1 to -10
+text = 'sivaprasad'  
+text[0]  
+'s'
+text[3]  
+'a'
+text[-1]  
+'d'
+text[-4]  
+'a'
+
+# print the character at slice of index 0 to 9 and -1 to -10
+text[0:10]
+'sivaprasad'
+text[0:4]
+'siva'
+text[4:10]
+'prasad'
+text[4:]
+'prasad'
+text[:4]
+'siva'
+text[-1:]
+'d'
---
+++ 8-9-2026/string/string.py
@@ -0,0 +1,14 @@
+# print ('hello world') 
+print ('hello siva prasad')
+# print msg using single quote
+print ('hello's)  ## got error because of single quote in hello's, so we can use double quote to avoid this error
+# print msg using double quote
+print ("hello's siva prasad") # using double quote to avoid error
+# print msg using escape character using single quote
+print('hello\'s siva prasad') 
+# print the msg using escape character
+print ('c:\python\test')
+# print the msg using \\ 
+print ('c:\\python\\test')
+
+print ("path: c:\\python\\test\\hello\\\"day1\".py")
